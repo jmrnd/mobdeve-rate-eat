b77ecd1a621d6438e5808d6767e50ce5b3a61c68
Abigail Seperidad <159636961+abigailseperidad@users.noreply.github.com>
2024-10-15 11:31:05 +0800

    home/landing page
    - search bar, nav bar, and view all restos combined
    - moved the content of all restos activity to the home fragment
    - changed the redirection of login from all restos activity to main activity
    - profile not yet implemented
    - enabled view binding in gradle
    - didn't do major designs yet
    - action bar is clickable but not typeable for search yet
    - search not implemented yet

--- app/src/main/java/com/example/rateeat/activities/MainActivity.java
+++ app/src/main/java/com/example/rateeat/activities/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.rateeat.activities;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -23,29 +25,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Set up the toolbar as the action bar
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setTitle("All Restaurants");
+
+        // Set up the onApplyWindowInsets listener for padding
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-
             int id = item.getItemId();
-
             if (id == R.id.home) {
                 replaceFragment(new HomeFragment());
+                getSupportActionBar().setTitle("All Restaurants");
             } else if (id == R.id.profile) {
                 replaceFragment(new ProfileFragment());
+                getSupportActionBar().setTitle("Profile");
             }
-
             return true;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // Get the current fragment
+        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.frameLayout);
+
+        // Check if the current fragment is ProfileFragment
+        if (currentFragment instanceof ProfileFragment) {
+            // Hide the search icon
+            menu.findItem(R.id.search).setVisible(false);
+        } else {
+            // Show the search icon
+            menu.findItem(R.id.search).setVisible(true);
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here
+        int id = item.getItemId();
+        // Implement action handling if necessary
+        return super.onOptionsItemSelected(item);
     }
 
     private void replaceFragment(Fragment fragment) {
