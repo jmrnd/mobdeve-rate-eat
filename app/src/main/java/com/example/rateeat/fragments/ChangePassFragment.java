d189c2588c93e67f7147b70bc9fec4576cc4aed0
Abigail Seperidad <159636961+abigailseperidad@users.noreply.github.com>
2024-10-19 23:24:17 +0800

    profile interactability
    - made the buttons interactive
    - alert dialog box when log out clicked
    - my reviews sample data changed

--- app/src/main/java/com/example/rateeat/fragments/ChangePassFragment.java
+++ app/src/main/java/com/example/rateeat/fragments/ChangePassFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,10 +14,31 @@ import com.example.rateeat.R;
 
 public class ChangePassFragment extends Fragment {
 
+    private Button saveButton;
+    private TextView forgotPass;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_change_pass, container, false);
+
+        saveButton = view.findViewById(R.id.saveButton);
+        forgotPass = view.findViewById(R.id.forgotPass);
+
+        saveButton.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "New password saved", Toast.LENGTH_SHORT).show();
+
+            // Go back to the previous fragment in the back stack
+            requireActivity().getSupportFragmentManager().popBackStack();
+        });
+
+        forgotPass.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "Redirect to forgot pass", Toast.LENGTH_SHORT).show();
+
+            // Go back to the previous fragment in the back stack
+            requireActivity().getSupportFragmentManager().popBackStack();
+        });
+
         return view;
     }
 }
--- app/src/main/java/com/example/rateeat/fragments/EditProfileFragment.java
+++ app/src/main/java/com/example/rateeat/fragments/EditProfileFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,10 +13,22 @@ import com.example.rateeat.R;
 
 public class EditProfileFragment extends Fragment {
 
+    private Button saveButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+
+        saveButton = view.findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "Successfully saved profile changes", Toast.LENGTH_SHORT).show();
+
+            // Go back to the previous fragment in the back stack
+            requireActivity().getSupportFragmentManager().popBackStack();
+        });
+
         return view;
     }
 }
